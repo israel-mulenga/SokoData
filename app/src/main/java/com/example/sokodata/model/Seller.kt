74ba2065ceff18311008f3c8872824cb0b2059ec
mulenga-nsembe-israel <mulengaisrael5@gmail.com>
2026-04-28 09:59:42 +0200

    class seller created

---
+++ app/src/main/java/com/example/sokodata/model/Seller.kt
@@ -0,0 +1,8 @@
+package com.example.sokodata.model;
+ data class Seller (
+  val id: String? = null,
+  val name: String,
+  val tableNumber: String,
+  val category: String,
+  val imageUrl: String? = null
+)
